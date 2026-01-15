9f60af71cf1edacb8c925d54c15ff2c21c9474ca
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-15 16:14:24 +0530

    [Prashant] add Program.cs file

---
+++ Map/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Collections_Generics.Map
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // WordFrequency obj1 = new WordFrequency();
+            // string filepath = "sample.txt";
+            // obj1.Count(filepath);
+
+            InvertMap obj2 = new InvertMap();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            dict.Add('A',1);
+            dict.Add('B',2);
+            dict.Add('C',1);
+            System.Console.WriteLine("Original Dictionary: ");
+            foreach(var pair in dict)
+            {
+                System.Console.WriteLine(pair.Key+"->"+pair.Value);
+            }
+            obj2.Invert(dict);
+        }
+    }
+}
