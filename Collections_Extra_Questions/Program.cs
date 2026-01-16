ca332447195c576a2b618e368f8a3aa634a03deb
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-16 22:39:59 +0530

    [Prashant] add Program.cs file

---
+++ Collections_Extra_Questions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Collections_Extra_Questions.BankTransactionFraudDetection;
+using Collections_Extra_Questions.ChatMessageCleanupSystem;
+using Collections_Extra_Questions.DriverAssignmentSystem;
+using Collections_Extra_Questions.InventoryReorderPrediction;
+using Collections_Extra_Questions.LibraryFineCalculation;
+using Collections_Extra_Questions.StudentPerformanceAnalyzer;
+namespace Collections_Extra_Questions
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // StudentAnalyzerMain.Execute();
+            // ChatCleanupMain.Execute();
+            // FraudDetectionMain.Execute();
+            // FineCalculatorMain.Execute();
+            // InventoryReorderMain.Execute();
+            CabAssignmentMain.Execute();
+        }
+    }
+}
