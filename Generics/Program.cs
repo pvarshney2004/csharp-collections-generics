1b140e586c51dedd7b1651124decb30cb8596a9e
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-16 12:37:30 +0530

    [Prashant] add Program.cs file for Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Generics.DynamicMarketPlace;
+using Generics.MealPlanGenerator;
+using Generics.ResumeScreeningSystem;
+using Generics.SmartWarehouseSystem;
+using Generics.UniversityCourseSystem;
+namespace Collections_Generics.Generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // SmartWarehouseMain.Execute();
+
+            // MarketPlaceMain.Execute();
+
+            // UniversitySystemMain.Execute();
+
+            // MealPlanMain.Execute();
+
+            ResumeMain.Execute();
+        }
+    }
+}
